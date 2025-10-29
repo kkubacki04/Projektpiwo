d13f2edc589d8ba2250066c690a768fb84513b7e
Kacper <kkubacki0404@gmail.com>
2025-10-29 05:28:12 +0100

    nowy register i poczatek profilu v2

--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from './supabaseClient';
+import { supabase } from '../supabaseClient';
 import LoginModal from './components/loginModal';
 import RegisterModal from './components/RegisterModal';
 import './index.css';
